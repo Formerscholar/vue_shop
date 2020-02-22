922b539f60235115a5090816968f89ac51324c15
Formerscholar <2319315514@qq.com>
2020-02-22 21:26:47 +0800

    2/22

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,7 @@
 <template>
-  $END$
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +10,20 @@
   }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .login_container {
+    height: 100%;
+    background-color: #2b4b6b;
+  }
 
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+import '@/assets/css/global.css'
 Vue.config.productionTip = false
 
-new Vue({
+ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
