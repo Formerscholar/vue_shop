3152d2810a2d5a379d543ab1ac6c41bc64658e98
Formerscholar <2319315514@qq.com>
2020-02-23 16:25:09 +0800

    完成了登录功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,61 @@
 <template>
   <div class="login_container">
-    <div class="login_box"></div>
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="~assets/logo.png">
+      </div>
+      <el-form ref="loginFormRef" label-width="0px" class="login_form" :model="loginFrom" :rules="rules">
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-user" v-model="loginFrom.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input prefix-icon="iconfont icon-3702mima" v-model="loginFrom.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "Login"
+    name: "Login",
+    data() {
+      return {
+        loginFrom: {
+          username: 'admin',
+          password: "123456"
+        },
+        rules: {
+          username: [
+            {required: true, message: '请输入登录名称', trigger: 'blur'},
+            {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
+          ],
+          password: [
+            {required: true, message: '请输入登录密码', trigger: 'blur'},
+            {min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur'}
+          ],
+        }
+      }
+    },
+    methods: {
+      resetLoginForm() {
+        this.$refs.loginFormRef.resetFields()
+      },
+      login() {
+        this.$refs.loginFormRef.validate(async vaild => {
+          if (!vaild) return;
+          const {data: res} = await this.$http.post('login', this.loginFrom)
+          if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+          this.$message.success(res.meta.msg)
+          window.sessionStorage.setItem("token", res.data.token)
+          this.$router.push('/home')
+        })
+      }
+    }
   }
 </script>
 
@@ -25,5 +74,38 @@
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
+  }
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+
+    img {
+      height: 100%;
+      width: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+
+  .btns {
+    display: flex;
+    justify-content: flex-end;
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-const Login = () => import('@/components/Login')
+const Login = () => import('components/Login')
+const Home = () => import('components/Home')
 
 
 Vue.use(VueRouter)
@@ -13,11 +14,26 @@ const routes = [
   {
     path:'/login',
     component:Login
-  }
+  },
+  {
+    path:'/home',
+    component:Home
+  },
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next)=>{
+  if (to.path === '/login'){
+    return next()
+  }
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr){
+    return  next('/login')
+  }
+  next()
 })
 
 export default router
